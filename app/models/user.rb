b99c6ba3efd38550dbdc2017d857c426805006a8
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-15 20:31:23 -0800

    user #mood done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+
+  def mood
+    happiness >= nausea ? "happy" : "sad"
+  end
 end
