1318e3fff5ac0ef057e04afd64fd1da9fe08f93f
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-15 20:29:16 -0800

    ride model finished

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,14 @@
 class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
+
+  def take_ride
+    rejections = []
+    rejections << "You do not have enough tickets to ride the #{attraction.name}." if user.tickets < attraction.tickets
+    rejections << "You are not tall enough to ride the #{attraction.name}." if user.height < attraction.min_height
+    return rejections.unshift("Sorry.").join(" ") if !rejections.empty?
+    user.update( tickets: user.tickets - attraction.tickets,
+                 nausea: user.nausea + attraction.nausea_rating,
+                 happiness: user.happiness + attraction.happiness_rating )
+  end
 end
