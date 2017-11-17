7f6a4757563adbccff52ced3c4e8de39fea3e7e5
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-17 13:32:44 -0800

    soln form div format looks better

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,3 +25,13 @@ class SessionsController < ApplicationController
   end
 
 end
+
+# def create
+#   @user = User.find_by(name: params[:user][:name])
+#   if @user && @user.authenticate(params[:user][:password])
+#     session[:user_id] = @user.id
+#     redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+#   else
+#     redirect_to signin_path
+#   end
+# end
